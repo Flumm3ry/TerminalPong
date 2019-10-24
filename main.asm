4148cf13497092e1da35695bb0f8b50fa9e6ba7d
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-10-24 17:49:15 +1100

    Updated .gitignore and added welcome message

---
+++ main.asm
@@ -0,0 +1,32 @@
+/* Pong in assembly terminal */
+
+/* constantes */
+.equ STD,	1
+.equ EXIT,	1
+.equ WRITE,	4
+.equ BUFFERSIZE,1
+
+/* Initialised data */
+.section .data
+welcomeMsg:
+	.ascii "Welcome to pong \n"
+welcomeLen = .- welcomeMsg
+
+/* Uniinitialised data */
+.section .bss
+.comm buffer, BUFFERSIZE
+
+.section .text
+.globl _start
+
+_start:
+mov r0, #STD
+ldr r1, =welcomeMsg
+ldr r2, =welcomeLen
+mov r7, #WRITE
+swi 0
+
+mov r0, #0
+mov r7, #1
+swi 0
+	
