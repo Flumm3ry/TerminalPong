22ba1144f64d387edae37809474861b045214647
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2019-10-26 14:28:31 +1100

    Added and tested draw screen function

--- main.asm
+++ main.asm
@@ -6,8 +6,13 @@
 .equ READ,	3
 .equ WRITE,	4
 .equ BUFFERSIZE,2
+.equ SCREENBUFFERSIZE,29
 .equ UP_CHAR,	119
 .equ DWN_CHAR,	115
+.equ WHITE_SPACE,32
+.equ PLAYER,	108
+.equ BALL,	254
+.equ NEW_LINE,	10
 
 /* Initialised data */
 .section .data
@@ -17,31 +22,26 @@ welcomeLen = .- welcomeMsg
 inputPromt:
 	.ascii "Enter a character to play:\n"
 inputPromtLen = .- inputPromt
-Player:
-	.ascii "l"
-Ball:
-	.ascii "O"
 
 /* Uniinitialised data */
 .section .bss
-.comm buffer, BUFFERSIZE
+.comm buffer, 		BUFFERSIZE
+.comm screenBuffer,	SCREENBUFFERSIZE
 
 .section .text
 .globl _start
 
 _start:
 
-mov sp,$1000
-
 bl DisplayWelcomeMessage
-//push {r4-r8}
+
+push {r4-r11}
 mov r0,#2
 mov r1,#3
-//push {r1}
-//mov r1,sp
 bl DrawScreen
-//pop {r1}
-//pop {r4-r8}
+pop {r4-r11}
+
+
 bl GetInput
 bl Exit
 
@@ -49,35 +49,50 @@ DrawScreen:
 	mov r4,r0	//player y	
 	//ldrb r5,[r1]	//ball x
 	mov r5,#2
-	lsl r1,#8
+	//lsl r1,#8
 	//ldrb r6,[r1]	//ball y
 		mov r6,#0
 
 	mov r8,#0		//current row
+	mov r9,#0		//current position in screen buffer
+	ldr r11,=screenBuffer
 	outerloop:
 		mov r7,#0	//current column
 		innerloop:
+			mov r10,#WHITE_SPACE
 			cmp r7,#0
 			bne checkball	//if we are not in the first row, it cannot be the player pos so check if ball pos
 			cmp r4,r8	//if the player y matches the current row, draw it
 			bne endofinnerloop
-			//Draw player
-			add r9,#1
+			mov r10,#PLAYER
 			b endofinnerloop
 			checkball:
 				cmp r7,r5
 				bne endofinnerloop	//if the Xs do not match go to the end of the loop
 				cmp r8,r6
 				bne endofinnerloop	//if the Ys do not match go to the end of the loop
-				//Draw ball
-				add r10,#1
+				mov r10,#BALL
 			endofinnerloop:
-		add r7,#1
-		cmp r7,#6
-		bne innerloop
-	add r8,#1
-	cmp r8,#6
+				strb r10,[r11,r9]
+				add r9,#1	
+				add r7,#1
+				cmp r7,#4
+			bne innerloop
+		mov r10,#NEW_LINE
+		strb r10,[r11,r9]
+		add r9,#1
+		add r8,#1
+		cmp r8,#6
 	bne outerloop
+	mov r10,#0
+	add r9,#1
+	strb r10,[r11,r9]
+	
+	mov r0,#STD
+	ldr r1,=screenBuffer
+	mov r2,#SCREENBUFFERSIZE
+	mov r7,#WRITE
+	swi 0
 bx lr
 
 DisplayWelcomeMessage:
